506a66c6aeb9affe120a50851ba7a8565b04e30d
Julio Vargas <jvdark2020@gmail.com>
2024-05-09 16:33:27 -0500

    Leader Board

---
+++ src/components/Dashboard/Leader/api/index.js
@@ -0,0 +1,32 @@
+export const DATA = [
+    {
+        id: 1,
+        name: "Billie Joe",
+        img: "",
+        score: 123
+    },
+    {
+        id: 2,
+        name: "King Low",
+        img: "",
+        score: 200
+    },
+    {
+        id: 3,
+        name: "Arthur Lewa",
+        img: "",
+        score: 178
+    },
+    {
+        id: 4,
+        name: "Louis Venga",
+        img: "",
+        score: 168
+    },
+    {
+        id: 5,
+        name: "Jessa Vhuea",
+        img: "",
+        score: 89
+    },
+];
